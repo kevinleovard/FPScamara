e38956843b3f45e8dd8170133873e05d8ea204da
kevinleovard <kevinleovard@gmail.com>
2019-11-12 20:10:35 -0500

    agregar unity
    se agregan los scripts

---
+++ fpscamara/Assets/FPSAim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSAim : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+public bool InvertedMouse;//variable booleana para invertir o no la camara//
+    float mouseX;// asignamos variable X//
+    float mouseY;// asignamos variable Y//
+    void Update()
+    {
+        Vector3 mousePosition=Input.mousePosition;//asignar posicion del mouse//
+        Debug.Log(mousePosition);// mostrar en pantalla la posicion del mouse//
+        Debug.Log(transform.localRotation);//mostrar en pantalla  la rotacion del objeto//
+        
+        mouseX += Input.GetAxis("Mouse X");//mostrar la posicion del eje X//
+
+        mouseY -= Input.GetAxis("Mouse Y");//accion falsa//
+        
+        Debug.Log(mouseX);//muestra la posicion del eje X//
+        transform.eulerAngles = new Vector3 (mouseY,mouseX,0);//
+
+    }
+}
---
+++ fpscamara/Assets/FPSmove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSmove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+public float speed = 0.1f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward*speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward*speed;            
+        }
+         if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right*speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right*speed;            
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            transform.position += transform.up*speed;
+
+        }
+    }
+}
